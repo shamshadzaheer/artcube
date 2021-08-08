3720070dd5fc1a44b0139a30cfb364616056f724
Shamshad Zaheer <shamshadtech101@gmail.com>
2021-08-08 12:01:33 -0700

    about and services

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container mb-5">
     <div class="text-center">
         <h1 class="page-title">About Us</h1>
     </div>
@@ -9,4 +9,28 @@
         <p>ArtCube is a team of passionate experts who purely enjoy working on the domain of video production and branding. We are the leading company in the field of Film Production, Branding and Events and have served hundreds of customersfrom the last couple of years. Quality work is not only our success but our core principle, which makes us work beyond the boundaries and extend our capabilities to work hard.</p>
     </div>
 </div>
+
+<div class="about-work-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <h3>1000</h3>
+                <p>Hours of Work</p>
+            </div>
+            <div class="col-md-3">
+                <h3>150</h3>
+                <p>Projects Completed</p>
+            </div>
+            <div class="col-md-3">
+                <h3>5</h3>
+                <p>Locations</p>
+            </div>
+            <div class="col-md-3">
+                <h3>10</h3>
+                <p>Awards Won</p>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
