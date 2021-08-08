af768c1b5fe7cd6b0017b59acf913827cea31967
Shamshad Zaheer <shamshadtech101@gmail.com>
2021-08-08 15:48:54 -0700

    home page added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Photo;
+use App\Models\Video;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $videos = Video::latest()->take(2)->get();
+        $photos = Photo::latest()->take(2)->get();
+
+        return view('home', compact('videos', 'photos'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,44 @@
 @section('content')
 <div class="container">
     <div class="home-video-box">
-        <div class="ro">
+        <div class="row">
             <div class="col">
                 <p>Afghanistan is a place of untold stories. Karukh, Herat</p>
-                <video width="320" height="240" controls>
-                    <source src="movie.mp4" type="video/mp4">
+                <video width="100%" autoplay controls>
+                    <source src="{{ url('video-files/main-video.mp4') }}" type="video/mp4">
                     Your browser does not support the video tag.
                 </video>
             </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="home-latest-videos">
+        <h2>Latest Videos:</h2>
+        <div class="row">
+            @foreach ($videos as $video)
+                <div class="col-md-6">
+                    <a href="{{ $video->getVideoLink() }}">
+                        <img src="{{ $video->getThumbnailPath() }}" alt="{{ $video->title }}">
+                    </a>
+                    <p>{{ $video->title }}</p>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+    <div class="home-latest-photos">
+        <h2>Latest Photos:</h2>
+        <div class="row">
+            @foreach ($photos as $photo)
+                <div class="col-md-6">
+                    <a href="{{ $photo->getUrl() }}" target="_blank">
+                        <img src="{{ $photo->getUrl() }}" alt="{{ $photo->title }}">
+                    </a>
+                    <p>{{ $photo->title }}</p>
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
